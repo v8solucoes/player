219b83388cfdf8b310f031cf84b93fbc4663c1a6
v8solucoes <solucoesv8@gmail.com>
2022-05-19 15:06:58 -0300

    novo

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+
 const canvas = document.getElementById("canvas");
+
 const image = new Image();
-image.src = './foto.png';
+image.src = 'https://picsum.photos/420/320';
 image.onload = drawImage;
 
 function drawImage() {
@@ -8,18 +10,25 @@ function drawImage() {
     canvas.height = image.naturalHeight;
     const ctx = canvas.getContext('2d');
     ctx.drawImage(image, 0, 0);
-}
+} 
 
 
 let video = document.getElementsByClassName('video')[0];
+
 
 function play() {
     video.play();
     document.getElementById('play').hidden = true;
     document.getElementById('canvas').hidden = true;
     document.getElementById('pause').hidden = false;
+    hover();
 
-
+}
+function playPosition() {
+    let player = document.querySelector('.play');
+    player.style.position = "absolute"
+    player.style.bottom = "0";
+    player.style.right = "0";
 }
 
 function pause() {
@@ -28,59 +37,74 @@ function pause() {
     document.getElementById('pause').hidden = true;
 }
 
+
+
 function right() {
-    let canva = document.querySelector('.canvas');
-    canva.style.position = "fixed";
-    canva.style.top = "60%";
-    canva.style.left = "71%";
-
+    let canvaPositionRight = document.querySelector('.canvas');
+    canvaPositionRight.style.position = "fixed";
+    canvaPositionRight.style.bottom = "0";
+    canvaPositionRight.style.right = "0";
+    canvaPositionRight.style.left = "";
+    canvaPositionRight.style.marginLeft = "";
 }
-
 function left() {
-    let canva = document.querySelector('.canvas');
-    canva.style.position = "fixed";
-    canva.style.top = "60%";
-    canva.style.left = "10px";
+    let canvaPositionleft = document.querySelector('.canvas');
+    canvaPositionleft.style.position = "fixed";
+    canvaPositionleft.style.bottom = "0";
+    canvaPositionleft.style.left = "0";
+    canvaPositionleft.style.right = "";
+    canvaPositionleft.style.marginLeft = "";
 }
 
 function middle() {
     let canva = document.querySelector('.canvas');
     canva.style.position = "fixed";
-    canva.style.top = "60%";
-    canva.style.left = "36.2%";
+    canva.style.bottom = "0";
+    canva.style.right = "";
+    canva.style.marginLeft = "calc(100% - 65%)";
 }
+
+
 
 function circle() {
     document.querySelector('.play').hidden = true;
     document.querySelector('.pause').hidden = true;
     document.querySelector('.video').hidden = true;
     document.querySelector('.close').hidden = true;
+    document.querySelector('.canvas1').hidden = true;
 
-    let canva = document.querySelector('.canvas');
-    canva.hidden = false;
-    canva.style.position = "fixed";
-    canva.style.top = "85%";
-    canva.style.left = "91%";
-    canva.style.borderRadius = "50%";
-    canva.style.width = "100px";
-    canva.style.height = "100px";
+
+    let floatCircle = document.querySelector(".canvas");
+    floatCircle.style.position = "fixed";
+    floatCircle.style.width = "100px";
+    floatCircle.style.height = "100px";
+    floatCircle.style.bottom = "0";
+    floatCircle.style.right = "0";
+    floatCircle.style.left = "";
 
     if (document.querySelector('.voltar') && document.querySelector('.imgFloat')) {
         document.getElementById('aumentar').hidden = false;
         document.querySelector('.imgFloat').hidden = false;
     } else {
-        back();
+        criaBotao();
         criaImagen();
         imgFloat();
         pause();
-        document.querySelector('.play').hidden = true;
-    }
+        document.querySelector('.play').hidden = false;
 
+    }
+    const hover = document.querySelector(".canvas");
+    hover.addEventListener('mouseover', function () {
+        document.getElementById('close').hidden = true;
+        document.getElementById('pause').hidden = true;
+    });
 }
+
+
 
 function imgFloat() {
     let image = document.createElement('img');
-    image.src = "./foto.png";
+    image.src = "https://picsum.photos/420/320";
     document.querySelector('.canvas').appendChild(image);
     image.style.borderRadius = "100%";
     image.style.width = "100px";
@@ -92,7 +116,7 @@ function imgFloat() {
 }
 
 
-function back() {
+function criaBotao() {
     let canva = document.createElement('button');
     document.querySelector('.canvas').appendChild(canva);
     canva.style.cursor = "pointer";
@@ -119,7 +143,6 @@ function back() {
     voltar.style.borderRadius = "50%";
     voltar.style.border = "none";
     voltar.style.background = "none";
-
 }
 
 function criaImagen() {
@@ -140,55 +163,43 @@ function criaImagen() {
 
 
 function ampliar() {
+    let canva = document.querySelector('.canvas');
+    canva.style.position = "relative";
+    canva.style.bottom = "0";
+    canva.style.right = "0";
+
     document.getElementById('aumentar').hidden = true;
     document.querySelector('.play').hidden = false;
-    document.querySelector('.pause').hidden = true;
+    document.querySelector('.pause').hidden = false;
     document.querySelector('.video').hidden = false;
-    document.getElementById('close').hidden = false;
+    document.querySelector('.close').hidden = false;
     document.querySelector('.canvas1').hidden = true;
     document.querySelector('.imgFloat').hidden = true;
-    let player = document.querySelector('.play');
-    player.style.top = "115px";
-    player.style.left = "170px";
+
+    let floatCircle = document.querySelector(".canvas");
+    floatCircle.style.width = "";
+    floatCircle.style.height = "";
     right();
 }
 
-let videoArea = document.querySelector('.canvas');
-videoArea.style.cursor = "move";
+const screenWidth1 = screen.width;
+if (screenWidth1 <= 600) {
+    middle();
+} else { }
 
 
-dragElement(document.querySelector('.canvas'));
+function hover() {
 
-function dragElement(elmnt) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(elmnt.id + "header")) {
-        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-    } else {
-        elmnt.onmousedown = dragMouseDown;
-    }
+    const hover = document.querySelector(".canvas");
+    hover.addEventListener('mouseover', function () {
+        document.getElementById('close').hidden = false;
+        document.getElementById('pause').hidden = false;
 
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
-    }
+    });
 
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    }
-
-    function closeDragElement() {
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
+    hover.addEventListener('mouseout', function () {
+        document.getElementById('close').hidden = true;
+        document.getElementById('pause').hidden = true;
+    });
 }
+
